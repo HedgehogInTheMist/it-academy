eb7a96bee991d1abd0eb2831f1f99b55fa49193f
Hedgehog <edovin.sergey@gmail.com>
2015-11-06 15:52:54 +0200

    threads

--- Threads/Solution.java
+++ Threads/Solution.java
@@ -1,23 +1,51 @@
 package Threads;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution extends Thread {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 
-		ThreadTest threadTest = new ThreadTest();
-
-		/*Thread t1 = new Thread(threadTest);
-		Thread t2 = new Thread(threadTest);*/
+		System.out.println("Enter \"start\" for starting threads or (q/Q) for Exit: ");
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		String userAnswer;
+		userAnswer = bufferedReader.readLine();
 		
-		Thread t1 = new ThreadTest();
-		Thread t2 = new ThreadTest();
+		while(true) {
+			switch (userAnswer) {
+			case "start": 
+						Thread t1 = new ThreadTest();
+						Thread t2 = new ThreadTest();
+								
+						t1.setName("Thread 1:");
+						t2.setName("Thread 2:");
 				
-		t1.setName("Thread 1:");
-		t2.setName("Thread 2:");
+						t2.start();
+						t1.start();
+						/*Thread.sleep(5000);
+						Thread.sleep(5000);*/
+										////Aaaarrrrgh!!! BULLSHIT
+				break;
+			case "q": 
+				break;
+			}
+		}
+}
 
-		t1.start();
-		//Thread.sleep(500);
-		t2.start();
-		// t2.join();
-						////Aaaarrrrgh!!! BULLSHIT
+	/**
+	 * Print numbers from 1 up to 100. Feeding 10 numbers in line separeted with space 
+	 */
+	//public synchronized static void print10() {
+	public static void print10() {
+		int number = 1;
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(Thread.currentThread().getName());
+			for (int j = 1; j <= 10; j++) {
+				System.out.print(" " + number + " ");
+				number++;
+			}
+			System.out.println();
+		}
 	}
 }
--- Threads/ThreadTest.java
+++ Threads/ThreadTest.java
@@ -1,19 +1,9 @@
 package Threads;
 
-public class ThreadTest extends Thread {
 
+public class ThreadTest extends Thread {
 	@Override
 	public void run() {
-		super.run();
-		int number = 1;
-		for (int i = 1; i <= 10; i++) {
-			System.out.print(Thread.currentThread().getName());
-			for (int j = 1; j <= 10; j++) {
-				System.out.print(" " + number + " ");
-				number++;
-			}
-			System.out.println();
-		}
-
+		Solution.print10();
 	}
 }
